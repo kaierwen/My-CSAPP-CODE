cb3ebaac9b11c4ca9c898ab95f8a077658f1dc27
kaierwen <mrkaierwen@gmail.com>
2022-06-23 14:30:26 +0800

    添加linker_order链接器顺序Demo

---
+++ C语言基础/linking链接/linker_order/static/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int square(int,int);
+
+int main() {
+    printf("result %d\n",square(1,2));
+    return 0;
+}
---
+++ C语言基础/linking链接/linker_order/static/square.c
@@ -0,0 +1,5 @@
+int add(int, int);
+
+int square(int a, int b) {
+    return add(a, b) * add(a, b);
+}
